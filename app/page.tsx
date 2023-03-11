f2f02e7684f2763737bcbc38e416b10e4ef072a2
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-11 18:06:33 +0530

    Add basic styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,21 @@
 import useUnsplash from "./hooks/useUnsplash";
 
 export default async function Home() {
-  const [url, author] = await useUnsplash();
+  const [url, author] = await useUnsplash(
+    "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=black"
+  );
 
   return (
     <main
-      className="h-screen bg-cover bg-center flex flex-col justify-between items-center text-slate-300"
+      className="h-screen bg-cover bg-center flex flex-col justify-between text-slate-300"
       style={{ backgroundImage: `url(${url})` }}
     >
-      <div>Crypto and weather here</div>
+      <div className="flex flex-row justify-between w-screen">
+        <p>Crypto</p>
+        <p>Weather</p>
+      </div>
 
-      <h1>TIME HERE</h1>
+      <h1 className="text-center text-4xl drop-shadow-2xl">TIME HERE</h1>
 
       <h1 className="text-xl text-slate-600 drop-shadow-md">{author}</h1>
     </main>
