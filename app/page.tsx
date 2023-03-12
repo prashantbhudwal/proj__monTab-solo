583236e4a2769de0bac191092726adf1980f96fa
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-12 18:48:49 +0530

    Displays a random pokemon on the tab

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,38 @@
+import usePokemon from "./hooks/usePokemon";
 import useUnsplash from "./hooks/useUnsplash";
+import Image from "next/image";
 
 export default async function Home() {
-  const unsplashUrl =
-    "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=black";
-  const [url, author] = await useUnsplash(unsplashUrl);
+  const test =
+    "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=japan";
+  const [unsplashUrl, author] = await useUnsplash(test);
+
+  const [pokemonUrl] = await usePokemon();
 
   return (
     <main
-      className="h-screen bg-cover bg-center flex flex-col justify-between text-slate-300"
-      style={{ backgroundImage: `url(${url})` }}
+      className="h-screen bg-cover bg-center flex flex-col justify-between text-slate-100 p-4"
+      style={{ backgroundImage: `url(${unsplashUrl})` }}
     >
-      <div className="flex flex-row justify-between w-screen">
+      <div className="flex flex-row justify-between">
         <p>Crypto</p>
         <p>Weather</p>
       </div>
 
-      <h1 className="text-center text-4xl drop-shadow-2xl">TIME HERE</h1>
+      <h1 className="text-center text-8xl drop-shadow-2xl">TIME HERE</h1>
 
-      <h1 className="text-xl text-slate-600 drop-shadow-md">{author}</h1>
+      <div className="flex flex-row justify-between items-baseline">
+        <h1 className="text-xl text-slate-600 drop-shadow-md">{author}</h1>
+        <div className="shadow-inner shadow-neutral-300 backdrop-filter backdrop-blur-sm backdrop-opacity-50 p-4 rounded-full flex items-center justify-center">
+          <Image
+            className=""
+            src={pokemonUrl}
+            alt="pokemon image"
+            width={200}
+            height={200}
+          ></Image>
+        </div>
+      </div>
     </main>
   );
 }
