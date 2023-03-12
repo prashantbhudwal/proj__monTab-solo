b52ffa78f30502de1db83d0617b063c98f4dca70
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-12 18:18:16 +0530

    Handle errors from pokemonAPI

--- app/hooks/usePokemon.tsx
+++ app/hooks/usePokemon.tsx
@@ -1,9 +1,15 @@
 export default async function usePokemon() {
   let randomNumber = Math.floor(Math.random() * 900) + 1;
-  const url = `https://pokeapi.co/api/v2/pokemon/${randomNumber}`;
-  const response = await fetch(url, { cache: "no-store" });
-  const json = await response.json();
-  const imageUrl = json.sprites.other.home.front_default;
-  console.log(imageUrl);
-  return [imageUrl];
+
+  try {
+    const url = `https://pokeapi.co/api/v2/pokemon/${randomNumber}`;
+    const response = await fetch(url);
+    const json = await response.json();
+    const imageUrl = json.sprites.other.home.front_default;
+    return [imageUrl];
+  } catch (error) {
+   return [
+     "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/39.png",
+   ]; 
+  }
 }
