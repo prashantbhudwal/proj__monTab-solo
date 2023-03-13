6e03078126c0280fe6f639f44478fed0a2daaefd
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-13 14:45:48 +0530

    Throw and error with a code

--- app/hooks/usePokemon.tsx
+++ app/hooks/usePokemon.tsx
@@ -5,12 +5,13 @@ export default async function usePokemon() {
     const url = `https://pokeapi.co/api/v2/pokemon/${randomNumber}`;
     const response = await fetch(url);
     if (!response.ok) {
-      throw Error("Something went wrong");
+      throw Error(`The request failed with status ${response.status} `);
     }
     const json = await response.json();
     const imageUrl = json.sprites.other.home.front_default;
     return [imageUrl];
   } catch (error) {
+    console.log(error);
     return [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/39.png",
     ];
