e79526dc3122f4b29c092672aed9ee7d9df44456
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-16 14:34:58 +0530

    Return weather description

--- app/hooks/useOpenWeather.tsx
+++ app/hooks/useOpenWeather.tsx
@@ -10,14 +10,15 @@ export default function useOpenWeather(lat: number | null, lon: number | null) {
         }
         return res.json();
       })
-      .then((data) => data.main);
+      .then((data) => data);
   };
   const { data: weather } = useSWR(() => (lat ? url : null), fetcher, {
     refreshInterval: 600000, // set refresh interval to 10 minutes
   });
   return [
-    weather?.temp || null,
-    weather?.feels_like || null,
-    weather?.humidity || null,
+    weather?.main.temp || null,
+    weather?.main.feels_like || null,
+    weather?.main.humidity || null,
+    weather?.weather[0].main || null,
   ];
 }
