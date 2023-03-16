8b12f7a65ba889b0fbc1823fced9426fca08b7a8
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-16 14:35:12 +0530

    Style weather widget

--- app/Weather.tsx
+++ app/Weather.tsx
@@ -2,19 +2,23 @@
 import useOpenWeather from "./hooks/useOpenWeather";
 import useGeoLocation from "./hooks/useGeoLocation";
 
-export default function Weather({ className }: { className?: string }) {
+export default function Weather() {
   const [lat, lon] = useGeoLocation();
-  const [temp, realFeel, humidity] = useOpenWeather(lat, lon);
+  const [temp, realFeel, humidity, desc] = useOpenWeather(lat, lon);
 
   if (!temp) {
     return <>Loading..</>;
   }
 
   return (
-    <div className={className}>
-      <p>Temp:{temp}</p>
-      <p>Real Feel:{realFeel}</p>
-      <p>Humidity:{humidity}</p>
+    <div className="text-fuchsia-500 shadow-inner backdrop-filter backdrop-blur-sm backdrop-opacity-50 shadow-fuchsia-500 rounded-lg p-4 drop-shadow-lg ">
+      <p className="text-3xl font-bold">
+        {Math.round(temp)}° <span className="text-sm">{desc}</span>
+      </p>
+      <p className="text-fuchsia-200 text-sm mt-2">
+        Real Feel: {Math.round(realFeel)}°C
+      </p>
+      <p className="text-fuchsia-200 text-sm mt-2">Humidity: {humidity}%</p>
     </div>
   );
 }
