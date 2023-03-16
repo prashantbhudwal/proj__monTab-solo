951850429da0e0959ee882a8bd29ddbe036f7b3e
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-16 14:54:41 +0530

    Improve spacing

--- app/Card.tsx
+++ app/Card.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export default function Card({ children }: { children: ReactNode }) {
   return (
-    <div className="text-fuchsia-500 shadow-inner backdrop-filter backdrop-blur-sm backdrop-opacity-50 shadow-fuchsia-500 rounded-lg p-4 drop-shadow-lg ">
+    <div className="text-fuchsia-500 shadow-inner backdrop-filter backdrop-blur-sm backdrop-opacity-50 shadow-fuchsia-500 rounded-lg px-8 py-4 drop-shadow-lg flex flex-col">
       {children}
     </div>
   );
